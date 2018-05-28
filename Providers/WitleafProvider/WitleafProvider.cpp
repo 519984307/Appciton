fdda49ab71791a9abbcd511bbbb4623c3d3a3d77
luoyuchun <luoyuchun@af64961a-2ac6-0a46-90b5-00df23af5d12>
2018-05-28 01:19:49 +0000

    更改IBPProvider出现的警告
    git-svn-id: https://192.168.0.2/svn/PMOS/trunk/src@313 af64961a-2ac6-0a46-90b5-00df23af5d12

--- Providers/WitleafProvider/WitleafProvider.cpp
+++ Providers/WitleafProvider/WitleafProvider.cpp
@@ -195,15 +195,15 @@ void WitleafProvider::handlePacket(unsigned char *data, int len)
         }
         case IBP_RSP_RESULT_DATA:
         {
-            short sys = (data[4] << 8) | data[5] == INVALID ? InvData() : (data[4] << 8) | data[5] - 100;
-            short dia = (data[6] << 8) | data[7] == INVALID ? InvData() : (data[6] << 8) | data[7] - 100;
-            short mean = (data[8] << 8) | data[9] == INVALID ? InvData() : (data[8] << 8) | data[9] - 100;
-            short pr = (data[10] << 8) | data[11] == INVALID ? InvData() : (data[10] << 8) | data[11] - 100;
+            short sys = ((data[4] << 8) | data[5]) == INVALID ? InvData() : ((data[4] << 8) | data[5]) - 100;
+            short dia = ((data[6] << 8) | data[7]) == INVALID ? InvData() : ((data[6] << 8) | data[7]) - 100;
+            short mean = ((data[8] << 8) | data[9]) == INVALID ? InvData() : ((data[8] << 8) | data[9]) - 100;
+            short pr = ((data[10] << 8) | data[11]) == INVALID ? InvData() : ((data[10] << 8) | data[11]) - 100;
             ibpParam.setRealTimeData(sys, dia, mean, pr, IBP_INPUT_1);
-            sys = (data[12] << 8) | data[13] == INVALID ? InvData() : (data[12] << 8) | data[13] - 100;
-            dia = (data[14] << 8) | data[15] == INVALID ? InvData() : (data[14] << 8) | data[15] - 100;
-            mean = (data[16] << 8) | data[17] == INVALID ? InvData() : (data[16] << 8) | data[17] - 100;
-            pr = (data[18] << 8) | data[19] == INVALID ? InvData() : (data[18] << 8) | data[19] - 100;
+            sys = ((data[12] << 8) | data[13]) == INVALID ? InvData() : ((data[12] << 8) | data[13]) - 100;
+            dia = ((data[14] << 8) | data[15]) == INVALID ? InvData() : ((data[14] << 8) | data[15]) - 100;
+            mean = ((data[16] << 8) | data[17]) == INVALID ? InvData() : ((data[16] << 8) | data[17]) - 100;
+            pr = ((data[18] << 8) | data[19]) == INVALID ? InvData() : ((data[18] << 8) | data[19]) - 100;
             ibpParam.setRealTimeData(sys, dia, mean, pr, IBP_INPUT_2);
             break;
         }
