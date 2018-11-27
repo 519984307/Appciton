e30f65a1899e39e5a6d27f3891251de0c9548c61
luoyuchun <luoyuchun@blmed.cn>
2018-11-27 09:27:23 +0800

    resp 增益设置(未验证)

--- Params/RESPParam/RESPSymbol.h
+++ Params/RESPParam/RESPSymbol.h
@@ -50,7 +50,7 @@ public:
     {
         static const char *symbol[RESP_ZOOM_NR] =
         {
-            "x1.0", "x2.0", "x3.0", "x4.0", "x5.0"
+            "x0.25", "x0.5", "x1.0", "x2.0", "x3.0", "x4.0", "x5.0"
         };
         return symbol[index];
     }
