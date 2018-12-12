edd60e784e4b6dd9efc4f491aa390d0cb6674948
duanzhonghuan <duanzhonghuan@blmed.cn>
2018-12-12 17:31:48 +0800

    wifi窗口强制不聚焦；调整wifi菜单大小

--- Components/Maintain/UserMaintain/WifiMaintainMenuContent.cpp
+++ Components/Maintain/UserMaintain/WifiMaintainMenuContent.cpp
@@ -25,7 +25,7 @@
 #include "LanguageManager.h"
 
 #define PROFILE_MAX_NUM 5
-#define LISTVIEW_MAX_VISIABLE_TIME 5
+#define LISTVIEW_MAX_VISIABLE_TIME 4
 
 class WifiMaintainMenuContentPrivate
 {
@@ -327,6 +327,9 @@ void WifiMaintainMenuContent::focusFirstItem()
 void WifiMaintainMenuContent::layoutExec()
 {
     Q_D(WifiMaintainMenuContent);
+
+    // 强制窗口不聚焦
+    this->setFocusPolicy(Qt::NoFocus);
 
     QHBoxLayout *hLayout;
     QVBoxLayout *layout = new QVBoxLayout(this);
