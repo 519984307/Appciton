e2b1f6ffac5a36c0258f1d296810980f658034cb
朱炜娟 <zhuweijuan@blmed.cn>
2019-01-07 18:34:22 +0800

    增加打印时病人信息单位

--- Components/Recorder/RecordPageGenerator.cpp
+++ Components/Recorder/RecordPageGenerator.cpp
@@ -34,6 +34,8 @@
 #include "RecorderManager.h"
 #include "TimeDate.h"
 #include "AlarmConfig.h"
+#include "UnitManager.h"
+#include "PatientManager.h"
 
 #define DEFAULT_PAGE_WIDTH 200
 #define PEN_WIDTH 2
@@ -154,14 +156,14 @@ RecordPage *RecordPageGenerator::createTitlePage(const QString &title, const Pat
     str = QString("%1: ").arg(trs("Weight"));
     if (patInfo.weight)
     {
-        str += QString("%1 %2").arg(QString::number(patInfo.weight)).arg(PatientSymbol::convert(patInfo.weightUnit));
+        str += QString("%1 %2").arg(QString::number(patInfo.weight)).arg(Unit::getSymbol(patientManager.getWeightUnit()));
     }
     infos.append(str);
 
     str = QString("%1: ").arg(trs("Height"));
     if (patInfo.height)
     {
-        str += QString::number(patInfo.height);
+        str += QString("%1 %2").arg(QString::number(patInfo.height)).arg(Unit::getSymbol(patientManager.getHeightUnit()));
     }
     infos.append(str);
 
