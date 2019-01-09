7ca738314fcc7525631187f1b7c3b0ad9cd85dc3
朱炜娟 <zhuweijuan@blmed.cn>
2019-01-09 11:27:29 +0800

    增加co2波形速度50mm/s

--- Params/CO2Param/CO2Widgets/CO2MenuContent.cpp
+++ Params/CO2Param/CO2Widgets/CO2MenuContent.cpp
@@ -188,6 +188,7 @@ void CO2MenuContent::layoutExec()
                        << CO2Symbol::convert(CO2_SWEEP_SPEED_62_5)
                        << CO2Symbol::convert(CO2_SWEEP_SPEED_125)
                        << CO2Symbol::convert(CO2_SWEEP_SPEED_250)
+                       << CO2Symbol::convert(CO2_SWEEP_SPEED_500)
                       );
     connect(comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(onComboBoxIndexChanged(int)));
     layout->addWidget(comboBox, d_ptr->combos.count(), 1);
