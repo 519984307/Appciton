1f7e00a333b6443f4cea4c6f82fd19891eef40ea
朱炜娟 <zhuweijuan@blmed.cn>
2019-01-21 15:58:06 +0800

    重新实现IThread里面的睡眠函数

--- Main/IThread.cpp
+++ Main/IThread.cpp
@@ -1,4 +1,19 @@
+/**
+ ** This file is part of the nPM project.
+ ** Copyright (C) Better Life Medical Technology Co., Ltd.
+ ** All Rights Reserved.
+ ** Unauthorized copying of this file, via any medium is strictly prohibited
+ ** Proprietary and confidential
+ **
+ ** Written by WeiJuan Zhu <zhuweijuan@blmed.cn>, 2019/1/21
+ **/
+
 #include "IThread.h"
+
+namespace IUnistd
+{
+#include "unistd.h"
+}
 
 /**************************************************************************************************
  * 功能： 主运行入口。
@@ -31,7 +46,25 @@ void IThread::stop(void)
     _isStop = true;
 debug("%s enter wait\n", qPrintable(_name));
     wait();
-debug("%s exit wait\n", qPrintable(_name));
+    debug("%s exit wait\n", qPrintable(_name));
+}
+
+void IThread::sleep(unsigned long d)
+{
+    using IUnistd::sleep;
+    IUnistd::sleep(d);
+}
+
+void IThread::msleep(unsigned long d)
+{
+    using IUnistd::usleep;
+    IUnistd::usleep(d * 1000);
+}
+
+void IThread::usleep(unsigned long d)
+{
+    using IUnistd::usleep;
+    IUnistd::usleep(d);
 }
 
 /**************************************************************************************************
@@ -49,5 +82,4 @@ IThread::IThread(const QString &name, iThreadEntry entry)
  *************************************************************************************************/
 IThread::~IThread()
 {
-
 }
