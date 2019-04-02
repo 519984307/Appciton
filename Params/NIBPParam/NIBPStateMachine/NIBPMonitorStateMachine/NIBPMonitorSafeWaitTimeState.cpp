8641ecd5520d8f92948b353504a0ca9c8626a2e9
方桐洲 <fangtongzhou@blmed.cn>
2019-04-02 11:25:31 +0800

    连续测量最后一个安全间隔期间到达计时时间，离开安全间隔时候进入原先测量模式

--- Params/NIBPParam/NIBPStateMachine/NIBPMonitorStateMachine/NIBPMonitorSafeWaitTimeState.cpp
+++ Params/NIBPParam/NIBPStateMachine/NIBPMonitorStateMachine/NIBPMonitorSafeWaitTimeState.cpp
@@ -219,7 +219,11 @@ void NIBPMonitorSafeWaitTimeState::handleNIBPEvent(NIBPEvent event, const unsign
             // 判断STAT倒计时是否在使用中
             if (nibpCountdownTime.isSTATMeasureTimeout())
             {
-                nibpCountdownTime.STATMeasureStart();  // 只测量5分钟。
+                NIBPMode mode = nibpParam.getSuperMeasurMode();
+                nibpParam.setSTATMeasure(false);
+                nibpParam.setMeasurMode(mode);
+                switchState(NIBP_MONITOR_STANDBY_STATE);
+                break;
             }
             switchState(NIBP_MONITOR_STARTING_STATE);
         }
