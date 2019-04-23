9b2127e12cd54e6114e1318275b9796654e42a41
陈冰云 <chenbingyun@blmed.cn>
2019-04-23 16:11:32 +0800

    修复Suntech 没有调用provider::feed接口导致一直会通信中断的问题

--- Providers/SuntechProvider/SuntechProvider.cpp
+++ Providers/SuntechProvider/SuntechProvider.cpp
@@ -441,7 +441,7 @@ SuntechProvider::SuntechProvider() :
 {
     UartAttrDesc portAttr(9600, 8, 'N', 1);
     initPort(portAttr);
-    setDisconnectThreshold(1);
+    setDisconnectThreshold(5);
 
     _timer = new QTimer();
     _timer->setInterval(200);
@@ -610,6 +610,7 @@ void SuntechProvider::_handlePacket(unsigned char *data, int len)
     // 当前压力
     case SUNTECH_RSP_CUFF_PRESSURE:
     {
+        feed();
         nibpParam.handleNIBPEvent(NIBP_EVENT_CURRENT_PRESSURE, &data[1], 2);
         int16_t pressure;
         pressure = (data[2] << 8) + data[1];
