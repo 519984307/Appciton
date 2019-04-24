7dd9dfdb2e2a033d95851fde8b0bed0c3d8111da
朱炜娟 <zhuweijuan@blmed.cn>
2019-04-24 17:13:54 +0800

    修改电池容量翻译

--- Components/Maintain/FactoryMaintain/MonitorInfoWindow.cpp
+++ Components/Maintain/FactoryMaintain/MonitorInfoWindow.cpp
@@ -125,7 +125,7 @@ void MonitorInfoWindow::layoutExec()
     d_ptr->labs.insert(MonitorInfoWindowPrivate
                        ::ITEM_LAB_CMLV_WORKTIME, labelRight);
 
-    labelLeft = new QLabel(trs("BatteryQuantity"));
+    labelLeft = new QLabel(trs("BatteryCapacity"));
     layout->addWidget(labelLeft, d_ptr->labs.count(), 0);
     labelRight = new QLabel("");
     labelRight->setAlignment(Qt::AlignCenter|Qt::AlignRight);
