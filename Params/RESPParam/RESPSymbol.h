2c972a7fc0a603c03497294adfd1dd1daaaea884
luoyuchun <luoyuchun@blmed.cn>
2019-04-29 18:12:48 +0800

    统一增益显示风格

--- Params/RESPParam/RESPSymbol.h
+++ Params/RESPParam/RESPSymbol.h
@@ -50,7 +50,7 @@ public:
     {
         static const char *symbol[RESP_ZOOM_NR] =
         {
-            "x0.25", "x0.5", "x1.0", "x2.0", "x3.0", "x4.0", "x5.0"
+            "X0.25", "X0.5", "X1", "X2", "X3", "X4", "X5"
         };
         return symbol[index];
     }
