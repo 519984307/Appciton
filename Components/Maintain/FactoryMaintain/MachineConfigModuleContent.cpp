f14a8743311355844b3149e5d9db6906e26905f2
方桐洲 <fangtongzhou@blmed.cn>
2019-06-06 19:09:14 +0800

    关闭新生儿进行NIBP测量

--- Components/Maintain/FactoryMaintain/MachineConfigModuleContent.cpp
+++ Components/Maintain/FactoryMaintain/MachineConfigModuleContent.cpp
@@ -48,7 +48,8 @@ public:
         ITEM_CBO_TSCREEN,
 #endif
         ITEM_CBO_BACKLIGHT,
-        ITEM_CBO_MAX
+        ITEM_CBO_MAX,
+        ITEM_CBO_NIBP_NEO_MEASURE
     };
 
     MachineConfigModuleContentPrivte()
@@ -182,6 +183,9 @@ void MachineConfigModuleContentPrivte::loadOptions()
     machineConfig.getNumValue("BacklightAdjustment", index);
     combos[ITEM_CBO_BACKLIGHT]->setCurrentIndex(index);
 
+    index = 0;
+    machineConfig.getNumValue("NIBPNEOMeasureEnable", index);
+    combos[ITEM_CBO_NIBP_NEO_MEASURE]->setCurrentIndex(index);
     itemInitMap = itemChangedMap;
 
 #ifdef HIDE_MACHINE_CONFIG_ITEMS
@@ -460,6 +464,24 @@ void MachineConfigModuleContent::layoutExec()
     connect(combo, SIGNAL(currentIndexChanged(int)), this, SLOT(onComboBoxIndexChanged(int)));
 
     layout->setRowStretch(d_ptr->combos.count(), 1);
+
+    label = new QLabel(trs("StopNeoNIBPMeasuer"));
+    layout->addWidget(label, d_ptr->combos.count(), 0);
+    combo = new ComboBox;
+    combo->blockSignals(true);
+    combo->addItems(QStringList()
+                    << trs("Off")
+                    << trs("On"));
+    combo->blockSignals(false);
+    layout->addWidget(combo, d_ptr->combos.count(), 1);
+    d_ptr->combos.insert(MachineConfigModuleContentPrivte
+                         ::ITEM_CBO_NIBP_NEO_MEASURE, combo);
+    itemId = MachineConfigModuleContentPrivte::ITEM_CBO_NIBP_NEO_MEASURE;
+    combo->setProperty("Item", qVariantFromValue(itemId));
+    connect(combo, SIGNAL(currentIndexChanged(int)), this, SLOT(onComboBoxIndexChanged(int)));
+
+    layout->setRowStretch(d_ptr->combos.count(), 1);
+
 }
 
 void MachineConfigModuleContent::hideEvent(QHideEvent *e)
@@ -570,6 +592,11 @@ void MachineConfigModuleContent::onComboBoxIndexChanged(int index)
             systemManager.setBrightness(br);
 #endif
             return;
+        }
+        case MachineConfigModuleContentPrivte::ITEM_CBO_NIBP_NEO_MEASURE:
+        {
+             enablePath = "NIBPNEOMeasureEnable";
+             break;
         }
         default:
             return;
