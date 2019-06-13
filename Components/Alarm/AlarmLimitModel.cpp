c256ec9b0db167175850568966e15f110e7812e1
朱炜娟 <zhuweijuan@blmed.cn>
2019-06-13 10:21:07 +0800

    co2去除低优先级

--- Components/Alarm/AlarmLimitModel.cpp
+++ Components/Alarm/AlarmLimitModel.cpp
@@ -124,7 +124,8 @@ bool AlarmLimitModel::setData(const QModelIndex &index, const QVariant &value, i
             case SECTION_LEVEL:
                 if (d_ptr->alarmDataInfos[row].paramID == PARAM_DUP_ECG
                         || d_ptr->alarmDataInfos[row].paramID == PARAM_SPO2
-                        || d_ptr->alarmDataInfos[row].paramID == PARAM_NIBP)
+                        || d_ptr->alarmDataInfos[row].paramID == PARAM_NIBP
+                        || d_ptr->alarmDataInfos[row].paramID == PARAM_CO2)
                 {
                     // ECG、SPO2、NIBP的报警优先级只有中级和高级
                     // 所以将设置值加一，保证中级存储为1,高级存储为2
@@ -250,7 +251,8 @@ QVariant AlarmLimitModel::data(const QModelIndex &index, int role) const
                 editInfo.type = ItemEditInfo::LIST;
                 if (d_ptr->alarmDataInfos.at(row).paramID == PARAM_DUP_ECG
                         || d_ptr->alarmDataInfos.at(row).paramID == PARAM_SPO2
-                        || d_ptr->alarmDataInfos.at(row).paramID == PARAM_NIBP)
+                        || d_ptr->alarmDataInfos.at(row).paramID == PARAM_NIBP
+                        || d_ptr->alarmDataInfos.at(row).paramID == PARAM_CO2)
                 {
                     // ECG、SPO2、NIBP的报警优先级只有中级和高级
                     editInfo.list << trs("Medium") << trs("High");
