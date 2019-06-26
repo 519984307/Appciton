9b2280d143a843eb3188f1ead134257569ebaa9f
方桐洲 <fangtongzhou@blmed.cn>
2019-06-26 17:25:25 +0800

    fix(print): 修改打印显示
    Closes #1750

--- Components/PatientData/EventStorageItem.cpp
+++ Components/PatientData/EventStorageItem.cpp
@@ -614,7 +614,7 @@ QString EventStorageItem::getEventTitle() const
     break;
     case EventCodeMarker:
     {
-        eventTitle = trs(QString(d_ptr->codeMarkerInfo->codeName));
+        eventTitle = QString(d_ptr->codeMarkerInfo->codeName);
     }
     break;
     case EventRealtimePrint:
