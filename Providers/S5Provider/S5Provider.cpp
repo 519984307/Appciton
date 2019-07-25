d22fba4bd799f044cf33a60d9f8ffbd3a02df2ab
朱炜娟 <zhuweijuan@blmed.cn>
2019-07-25 09:32:10 +0800

    fix(spo2): 弱冠注的判断只是pi<1
    fix #2064

--- Providers/S5Provider/S5Provider.cpp
+++ Providers/S5Provider/S5Provider.cpp
@@ -330,7 +330,13 @@ bool S5Provider::isResult_BAR(unsigned char *packet)
     // PI;
     bool isLowPerfusion = false;
     short piValue = packet[15];
-    if (piValue > 200 || piValue < 1)
+    if (piValue > 200)
+    {
+        piValue = InvData();
+        spo2Param.setPerfusionStatus(false);
+        isLowPerfusion = false;
+    }
+    else if (piValue < 1)
     {
         piValue = InvData();
         spo2Param.setPerfusionStatus(true);
