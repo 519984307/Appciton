5e0714b43e60ab26e123e87d692ec1dc8bdac4c4
方桐洲 <fangtongzhou@blmed.cn>
2019-09-17 21:33:01 +0800

    fix(t5): 优化代码
    Closes #2466

--- Providers/T5Provider/T5Provider.cpp
+++ Providers/T5Provider/T5Provider.cpp
@@ -443,18 +443,14 @@ void T5Provider::_sensorOff(unsigned char *packet)
     static bool sensorHasContected1 = false;  // 开机之后有连接断开才会报探头脱落
     static bool sensorHasContected2 = false;
 
-    if ((!sensorHasContected1 || !sensorHasContected2) && packet[1] == 0x00)
+    if (!sensorHasContected1 && packet[1] & 0x02)
     {
         sensorHasContected1 = true;
-        sensorHasContected2 = true;
     }
-    else if (!sensorHasContected2 && packet[1] == 0x01)
+
+    if (!sensorHasContected2 && packet[1] & 0x01)
     {
         sensorHasContected2 = true;
-    }
-    else if (!sensorHasContected1 && packet[1] == 0x02)
-    {
-        sensorHasContected1 = true;
     }
 
     if (packet[1] == 0x00)
