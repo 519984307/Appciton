2f7517231bd9443d8e7f69dbc45cca6fdc1486be
lianghuan <lianghuan@blmed.cn>
2020-07-14 09:22:02 +0800

    fix(ibp provider): 调整SmartIBPProvider中setChannelData中PR值判断条件

--- Providers/SmartIBPProvider/SmartIBPProvider.cpp
+++ Providers/SmartIBPProvider/SmartIBPProvider.cpp
@@ -189,7 +189,7 @@ public:
             chn->map = validPressureValue(chn, val) ? val : InvData();
             break;
         case DATA_TYPE_PR:
-            chn->pr = val == INVALID_MEASURE_VALUE ? InvData() : val;
+            chn->pr = val != INVALID_MEASURE_VALUE ? val : InvData();
             break;
         default:
             break;
