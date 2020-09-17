d6f2413bf7a2baf5ecbedbb041538866e4284a0c
lianghuan <lianghuan@blmed.cn>
2020-09-17 14:30:22 +0800

    feat(software version): 根据DV技术要求，修改DV软件版本号显示

--- Components/Maintain/FactoryMaintain/SoftwareVersionWindow.cpp
+++ Components/Maintain/FactoryMaintain/SoftwareVersionWindow.cpp
@@ -16,7 +16,26 @@
 #include <QVBoxLayout>
 #include <QGridLayout>
 #include "Framework/Language/LanguageManager.h"
+#include "IConfig.h"
 
+/*
+ * DV SW完整版本号命名规则
+ * DV SW Version: DBA1.0.0.A.CN
+ * D ------------------------ 产品类别设别号。“D”表示监护设备。
+ * B ------------------------ 注册单元设别号。“B”表示产品属于B注册单元；新生儿专用机器为A注册单元。
+ * A ------------------------ 产品设别号。 D6、D7、D8、D6S、D7S、D8S用“A”表示。
+ * 1 ------------------------ 重大增强类软件更新设别号。从“1”开始。
+ * 0 ------------------------ 轻微增强类软件更新设别号。从"0”开始。
+ * 0 ------------------------ 纠正类软件更新设别号。从"0”开始。
+ * A ------------------------ 软件构建识别号。 “A”表示主程序,“B”表示副程序,如有多个副程序,则用“B”、“C”、“D”......分别表示。
+ * CN------------------------ 语种识别号。“CN”表示中文或通用,“EN”表示英文。具体按照公司内部“软件语种识别号规定”执行。
+ *
+ * 详细软件版本信息查看戴维产品技术要求
+ * */
+#define DV_RELEASE_VERSION "DBA1"
+#define DV_RELEASE_VERSION_NEO "DAA1"    // 新生儿专用机器发布版本
+#define DV_SW_VERSION "DBA1.0.0.A.CN"
+#define DV_SW_VERSION_NEO "DAA1.0.0.A.CN"    // 新生儿专用机器软件版本
 SoftwareVersionWindow::SoftwareVersionWindow(): Dialog()
 {
     layoutExec();
@@ -42,14 +61,31 @@ void SoftwareVersionWindow::layoutExec()
     label = new QLabel(QString("%1:").arg(trs("ReleaseVersion")));
     glayout->addWidget(label, 0, 1, Qt::AlignLeft | Qt::AlignHCenter);
     label = new QLabel;
-    QString softwaveVersion = QString(GIT_VERSION).left(2);
+    QString softwaveVersion;
+    int isNeoMachine = 0;
+    machineConfig.getNumValue("NeonateMachine", isNeoMachine);
+    if (isNeoMachine)
+    {
+        softwaveVersion = DV_RELEASE_VERSION_NEO;
+    }
+    else
+    {
+        softwaveVersion = DV_RELEASE_VERSION;
+    }
     label->setText(softwaveVersion);
     glayout->addWidget(label, 0, 2, Qt::AlignLeft | Qt::AlignHCenter);
 
     label = new QLabel(QString("%1:").arg(trs("SoftwareVersion")));
     glayout->addWidget(label, 1, 1, Qt::AlignLeft | Qt::AlignHCenter);
     label = new QLabel;
-    softwaveVersion = QString(GIT_VERSION);
+    if (isNeoMachine)
+    {
+        softwaveVersion = DV_SW_VERSION_NEO;
+    }
+    else
+    {
+        softwaveVersion = DV_SW_VERSION;
+    }
     label->setText(softwaveVersion);
     glayout->addWidget(label, 1, 2, Qt::AlignLeft | Qt::AlignHCenter);
 
