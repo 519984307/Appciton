280ac5b64cbcf7d3dc7bb140dd458eb87b6119ce
lianghuan <lianghuan@blmed.cn>
2021-04-27 18:09:59 +0800

    feat(pi trend): 参考SPO2双血氧参数布局，修改PI参数显示布局

--- Params/SPO2Param/SPO2Widgets/PITrendWidget.cpp
+++ Params/SPO2Param/SPO2Widgets/PITrendWidget.cpp
@@ -21,34 +21,48 @@ class PITrendWidgetPrivate
 {
 public:
     PITrendWidgetPrivate()
-        : piValue(NULL),
-          scale(1), isAlarm(false)
+        : piValue(NULL), pluginPiName(NULL), pluginPiValue(NULL),
+          scale(1), isAlarm(false), isPluginPIAlarm(false)
     {}
     ~PITrendWidgetPrivate(){}
 
     QLabel *piValue;
+    QLabel *pluginPiName;       // plugin module pi name and value
+    QLabel *pluginPiValue;
     short scale;
     bool isAlarm;
+    bool isPluginPIAlarm;
     QString piString;
+    QString pluginPIString;
 };
 
-void PITrendWidget::setPIValue(int16_t pi)
+void PITrendWidget::setPIValue(short pi, bool isPlugin)
 {
+    QString valueStr;
     if (0 <= pi && pi < 1000)
     {
         // PI value range 0%-9.99%, set the resolution to 0.01
-        d_ptr->piString = QString::number(pi / (d_ptr->scale * 1.0), 'f', 2);
+        valueStr = QString::number(pi / (d_ptr->scale * 1.0), 'f', 2);
     }
     else if (pi >= 1000 && pi <= 10000)
     {
         // PI value range 10.0%-100%, set the resolution to 0.1
-        d_ptr->piString = QString::number(pi / (d_ptr->scale * 1.0), 'f', 1);
+        valueStr = QString::number(pi / (d_ptr->scale * 1.0), 'f', 1);
     }
     else
     {
-        d_ptr->piString = InvStr();
+        valueStr = InvStr();
     }
-    d_ptr->piValue->setText(d_ptr->piString);
+    if (!isPlugin)
+    {
+        d_ptr->piString = valueStr;
+        d_ptr->piValue->setText(d_ptr->piString);
+    }
+    else
+    {
+        d_ptr->pluginPIString = valueStr;
+        d_ptr->pluginPiValue->setText(d_ptr->piString);
+    }
 }
 
 void PITrendWidget::updateLimit()
@@ -61,6 +75,11 @@ void PITrendWidget::isAlarm(bool flag)
     d_ptr->isAlarm = flag;
 }
 
+void PITrendWidget::isPluginPIAlarm(bool flag)
+{
+    d_ptr->isPluginPIAlarm = flag;
+}
+
 void PITrendWidget::showValue()
 {
     QPalette psrc;
@@ -69,6 +88,17 @@ void PITrendWidget::showValue()
     {
         showAlarmStatus(d_ptr->piValue);
         showAlarmParamLimit(d_ptr->piValue, d_ptr->piString, psrc);
+        restoreNormalStatusLater();
+    }
+    else
+    {
+        showNormalStatus(psrc);
+    }
+
+    if (d_ptr->isPluginPIAlarm && d_ptr->pluginPIString != InvStr())
+    {
+        showAlarmStatus(d_ptr->pluginPiValue);
+        showAlarmParamLimit(d_ptr->pluginPiValue, d_ptr->pluginPIString, psrc);
         restoreNormalStatusLater();
     }
     else
@@ -88,7 +118,22 @@ PITrendWidget::PITrendWidget()
     d_ptr->piValue->setAlignment(Qt::AlignCenter);
     d_ptr->piValue->setText(InvStr());
 
-    contentLayout->addWidget(d_ptr->piValue, 5);
+    QHBoxLayout *pluginLayout = new QHBoxLayout();
+
+    d_ptr->pluginPiName = new QLabel(trs(paramInfo.getSubParamName(SUB_PARAM_PLUGIN_PI)));
+    d_ptr->pluginPiName->setAlignment(Qt::AlignCenter);
+    pluginLayout->addWidget(d_ptr->pluginPiName);
+    d_ptr->pluginPiValue = new QLabel();
+    d_ptr->pluginPiValue->setAlignment(Qt::AlignCenter);
+    d_ptr->pluginPiValue->setText(InvStr());
+    pluginLayout->addWidget(d_ptr->pluginPiValue);
+
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->setMargin(10);
+    layout->addWidget(d_ptr->piValue);
+    layout->addLayout(pluginLayout);
+
+    contentLayout->addLayout(layout, 7);
     connect(this, SIGNAL(released()), this, SLOT(onRelease()));
 
     updateLimit();
@@ -116,10 +161,24 @@ void PITrendWidget::doRestoreNormalStatus()
 void PITrendWidget::setTextSize()
 {
     QRect r = this->rect();
-    r.adjust(nameLabel->width(), 0, 0, 0);
+    if (d_ptr->pluginPiName->isVisible() && d_ptr->pluginPiValue->isVisible())
+    {
+        r.adjust(nameLabel->width() * 3, 0, 0, 0);
+        r.setHeight(r.height() / 2);
+    }
+    else
+    {
+        r.adjust(nameLabel->width(), 0, 0, 0);
+    }
+
     int fontsize = fontManager.adjustNumFontSize(r, true, "99999");
     QFont font = fontManager.numFont(fontsize, true);
     d_ptr->piValue->setFont(font);
+    d_ptr->pluginPiValue->setFont(font);
+
+    int fontSize = fontManager.getFontSize(3);
+    font = fontManager.textFont(fontSize);
+    d_ptr->pluginPiName->setFont(font);
 }
 
 void PITrendWidget::onRelease()
